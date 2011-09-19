af46eb6196c67e1794e51a8d8bd738e1c9fbdf65
René Bohne <rene.bohne@gmail.com>
2011-09-19 11:13:30 +0200

    added pins.h - pin mapping for my mbed breakout board

---
+++ pins.h
@@ -0,0 +1,34 @@
+#define X_STEP_PIN         p5
+#define X_DIR_PIN          p6
+#define X_ENABLE_PIN       p7
+#define X_MIN_PIN           NC
+#define X_MAX_PIN           NC
+
+
+#define Y_STEP_PIN         p10
+#define Y_DIR_PIN          p9
+#define Y_ENABLE_PIN       p8
+#define Y_MIN_PIN          NC
+#define Y_MAX_PIN          NC
+
+
+#define Z_STEP_PIN         p16
+#define Z_DIR_PIN          p15
+#define Z_ENABLE_PIN       p14
+#define Z_MIN_PIN          NC
+#define Z_MAX_PIN          NC
+
+#define E_STEP_PIN         p11
+#define E_DIR_PIN          p12
+#define E_ENABLE_PIN       p13
+
+#define LED_PIN            NC
+
+#define FAN_PIN            NC
+
+#define PS_ON_PIN          NC
+#define KILL_PIN           NC
+
+#define HEATER_0_PIN p21
+
+#define TEMP_0_PIN p20
